bf35253e41733bd683d14e56808d888115a1b9f8
Davidos-wq <davidaliev2017@gmail.com>
2026-07-15 17:18:08 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-
-def count_twosum(a,b):
-    return(a+b)
-
-if __name__ == "__main__":
-   first_elem = input("Введіть перше значення")
-   second_elem = input("Введіть друге значення")
-   count_twosum()
